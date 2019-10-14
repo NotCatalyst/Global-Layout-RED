fb91adc8863c200cfb1c947409c0241e09a73025
H3x404 <50049926+H3x404@users.noreply.github.com>
2019-10-14 03:01:12 -0300

    ~ Tasks list.

---
+++ tasks.php
@@ -0,0 +1,64 @@
+<?php require_once 'engine/init.php'; include 'layout/overall/header.php'; ?>
+
+                        <br>        
+</tbody></table>
+<br>
+<table bgcolor="#F2EEE3" border="0" cellpadding="0" cellspacing="2" width="100%">  
+<tbody><tr bgcolor="#0B5A69"><td class="white" colspan="5"><b>Task System (Edryn NPC)</b></td></tr>  
+<tr><td><table border="0" cellpadding="2" cellspacing="1" width="100%">  
+<tbody><tr bgcolor="#eeddb9"><td width="10%"><b>Images</b></td><td width="50%"><b>Kills:</b></td><td width="30%"><b>Reward:</b></td></tr>  
+
+<tr class="KL" bgcolor="#eeddb9"><td height="100" width="100"><center><img src="images/task/rotworm.gif"></center></td><td><center><br>100 Rotworms</b></center></td><td><center> <li>200,000 Experience points</li><li>5,000 Gold coins</li></center></td></tr>
+  
+
+<tr class="KL" bgcolor="#eeddb9"><td height="100" width="100"><center><img src="images/task/vampire.gif"></center></td><td><center><br>600 Vampires</b></center></td><td><center> <li>850,000 Experience points</li></center></td></tr>
+
+<tr class="KL" bgcolor="#eeddb9"><td height="100" width="100"><center><img src="images/task/necromancer.gif"></center></td><td><center><br>500 Necromancers</b></center></td><td><center> <li>600,000 Experience points</li><li>Soul Stone</li></center></td></tr>
+
+<tr class="KL" bgcolor="#eeddb9"><td height="100" width="100"><center><img src="images/task/quara.gif"></center></td><td><center><br>400 Quaras (All kinds)</b></center></td><td><center> <li>750,000 Experience points</li><li>20,000 Gold coins</li></center></td></tr>
+
+<tr class="KL" bgcolor="#eeddb9"><td height="100" width="100"><center><img src="images/task/lizard.gif"></center></td><td><center><br>600 Lizards (All Kinds)</b></center></td><td><center> <li>1,000,000 Experience points</li></center></td></tr>
+
+<tr class="KL" bgcolor="#eeddb9"><td height="100" width="100"><center><img src="images/task/dragon.gif"></center></td><td><center><br>1200 Dragons</b></center></td><td><center> <li>1,200,000 Experience points</li></center></td></tr>
+
+<tr class="KL" bgcolor="#eeddb9"><td height="100" width="100"><center><img src="images/task/bograider.gif"></center></td><td><center><br>1111 Bog Raiders</b></center></td><td><center> <li>1,250,000 Experience points</li><li>50,000 Gold coins</li></center></td></tr>
+
+<tr class="KL" bgcolor="#eeddb9"><td height="100" width="100"><center><img src="images/task/giantspider.gif"></center></td><td><center><br>500 Giant Spiders</b></center></td><td><center> <li>1,000,000 Experience points</li><li>30,000 Gold coins</li></center></td></tr>
+
+<tr class="KL" bgcolor="#eeddb9"><td height="100" width="100"><center><img src="images/task/dragonlord.gif"></center></td><td><center><br>1111 Dragon Lords</b></center></td><td><center> <li>1,500,000 Experience points</li><li>Dragon Claw</li></center></td></tr>
+
+<tr class="KL" bgcolor="#eeddb9"><td height="100" width="100"><center><img src="images/task/frostdragon.gif"></center></td><td><center><br>1111 Frost Dragons</b></center></td><td><center> <li>1,750,000 Experience points</li></center></td></tr>
+
+<tr class="KL" bgcolor="#eeddb9"><td height="100" width="100"><center><img src="images/task/hydra.gif"></center></td><td><center><br>800 Hydras</b></center></td><td><center> <li>1,400,000 Experience points</li></center></td></tr>
+
+<tr class="KL" bgcolor="#eeddb9"><td height="100" width="100"><center><img src="images/task/nightmare.gif"></center></td><td><center><br>800 Nightmares</b></center></td><td><center> <li>1,450,000 Experience points</li></center></td></tr>
+
+<tr class="KL" bgcolor="#eeddb9"><td height="100" width="100"><center><img src="images/task/behemoth.gif"></center></td><td><center><br>400 Behemoths</b></center></td><td><center> <li>1,100,000 Experience points</li></center></td></tr>
+
+<tr class="KL" bgcolor="#eeddb9"><td height="100" width="100"><center><img src="images/task/hellspawn.gif"></center></td><td><center><br>600 Hellspawns</b></center></td><td><center> <li>1,100,000 Experience points</li><li>Nose Ring</li></center></td></tr>
+
+<tr class="KL" bgcolor="#eeddb9"><td height="100" width="100"><center><img src="images/task/infernalist.gif"></center></td><td><center><br>400 Infernalists</b></center></td><td><center> <li>1,000,000 Experience points</li></center></td></tr>
+
+<tr class="KL" bgcolor="#eeddb9"><td height="100" width="100"><center><img src="images/task/draken.gif"></center></td><td><center><br>800 Drakens (All kinds)</b></center></td><td><center> <li>2,000,000 Experience points</li></center></td></tr>
+
+<tr class="KL" bgcolor="#eeddb9"><td height="100" width="100"><center><img src="images/task/phantasm.gif"></center></td><td><center><br>360 Phantasms</b></center></td><td><center> <li>1,200,000 Experience points</li></center></td></tr>
+
+<tr class="KL" bgcolor="#eeddb9"><td height="100" width="100"><center><img src="images/task/fury.gif"></center></td><td><center><br>666 Furys</b></center></td><td><center> <li>1,500,000 Experience points</li><li>70,000 Gold coins</li></center></td></tr>
+
+<tr class="KL" bgcolor="#eeddb9"><td height="100" width="100"><center><img src="images/task/darktorturer.gif"></center></td><td><center><br>300 Dark Torturers</b></center></td><td><center> <li>1,300,000 Experience points</li><li>Mr Punish's Handcuffs</li></center></td></tr>
+
+<tr class="KL" bgcolor="#eeddb9"><td height="100" width="100"><center><img src="images/task/handofc.gif"></center></td><td><center><br>300 Hands of cursed Fate</b></center></td><td><center> <li>1,300,000 Experience points</li><li>Handmaiden's Protector</li></center></td></tr>
+
+<tr class="KL" bgcolor="#eeddb9"><td height="100" width="100"><center><img src="images/task/grimreaper.gif"></center></td><td><center><br>600 Grim Reapers</b></center></td><td><center> <li>1,400,000 Experience points</li><li>100,000 Gold coins</li></center></td></tr>
+
+<tr class="KL" bgcolor="#eeddb9"><td height="100" width="100"><center><img src="images/task/demon.gif"></center></td><td><center><br>666 Demons</b></center></td><td><center> <li>2,000,000 Experience points</li></center></td></tr>
+
+<tr class="KL" bgcolor="#eeddb9"><td height="100" width="100"><center><img src="images/task/hellhound.gif"></center></td><td><center><br>500 Hellhounds</b></center></td><td><center> <li>2,000,000 Experience points</li></center></td></tr>
+
+<tr class="KL" bgcolor="#eeddb9"><td height="100" width="100"><center><img src="images/task/undeaddragon.gif"></center></td><td><center><br>500 Undead Dragons</b></center></td><td><center> <li>1,800,000 Experience points</li></center></td></tr>
+
+<tr class="KL" bgcolor="#eeddb9"><td height="100" width="100"><center><img src="images/task/juggernaut.gif"></center></td><td><center><br>500 Juggernauts</b></center></td><td><center> <li>3,750,000 Experience points</li></center></td></tr></tr>
+
+</tbody></table></td></tr>  
+</tbody></table>                                 
+<?php include 'layout/overall/footer.php'; ?>
