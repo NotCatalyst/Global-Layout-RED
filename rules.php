dcef625f9f7d6bfd105ad8814d442e5cd2bafc5e
H3x404 <50049926+H3x404@users.noreply.github.com>
2019-10-14 03:21:49 -0300

    ~ Server rules.

---
+++ rules.php
@@ -0,0 +1,161 @@
+<?php require_once 'engine/init.php'; include 'layout/overall/header.php'; ?>
+</center><br /><div class="TableContainer"><table class="Table5" cellpadding="0" cellspacing="0"><div class="CaptionContainer"><div class="CaptionInnerContainer"><span class="CaptionEdgeLeftTop" style="background-image:url(layouts/tibiacom/images/content/box-frame-edge.gif);" /></span><span class="CaptionEdgeRightTop" style="background-image:url(layouts/tibiacom/images/content/box-frame-edge.gif);" /></span><span class="CaptionBorderTop" style="background-image:url(layouts/tibiacom/images/content/table-headline-border.gif);" ></span><span class="CaptionVerticalLeft" style="background-image:url(layouts/tibiacom/images/content/box-frame-vertical.gif);" /></span><div class="Text">Verana Rules </div><span class="CaptionVerticalRight" style="background-image:url(layouts/tibiacom/images/content/box-frame-vertical.gif);" /></span><span class="CaptionBorderBottom" style="background-image:url(layouts/tibiacom/images/content/table-headline-border.gif);" ></span><span class="CaptionEdgeLeftBottom" style="background-image:url(layouts/tibiacom/images/content/box-frame-edge.gif);" /></span><span class="CaptionEdgeRightBottom" style="background-image:url(layouts/tibiacom/images/content/box-frame-edge.gif);" /></span></div></div><tr><td><div class="InnerTableContainer"><table width="100%"><tr><td>
+
+		<div>
+			<input type="checkbox" class="read-more-state" id="post-1" />
+			<font class="rule-title">Multiclient in PVP</font><br />
+			<font class="rule-description">&#x2022; You may not control more than one character at a time in PvP, either through the use of multiple computers or bots. Each character in the fight must be controlled by a unique player. This includes healing your characters, untrapping yourself, blocking pathways or any other way that changes the outcome of the fight.</font>
+			<div class="read-more-wrap">
+				<span class="read-more-target"><br />
+					
+<span class="rule-yes"><i class="fa fa-thumbs-up" aria-hidden="true"></i> You are allowed to play with multiple characters at once.</span><br />
+<br />
+<span class="rule-no"><i class="fa fa-thumbs-down" aria-hidden="true"></i> You can't bring your second character to help kill other players.</span><br />
+<span class="rule-no"><i class="fa fa-thumbs-down" aria-hidden="true"></i> You can't bring your second character to heal yourself, move items or block pathways even if you don't use the character to attack.</span><br />
+					<br />
+				</span>
+			</div>
+
+			<label for="post-1" class="read-more-trigger"></label>
+		</div>
+		<hr class="rule-divider" />
+		
+
+		<div>
+			<input type="checkbox" class="read-more-state" id="post-2" />
+			<font class="rule-title">PVP Abuse</font><br />
+			<font class="rule-description">&#x2022; Do not take a low level character into a war and suicide to give other players a red skull. Do not bring a low level character to block magic walls or interfere with a war.</font>
+			<div class="read-more-wrap">
+				<span class="read-more-target"><br />
+					
+<span class="rule-yes"><i class="fa fa-thumbs-up" aria-hidden="true"></i> You are allowed to participate in any war between other players or guilds even if you're not invited.</span><br />
+<span class="rule-yes"><i class="fa fa-thumbs-up" aria-hidden="true"></i> You are allowed to block off boats/entrances while you're intending to fight other players or losing skull.</span><br />
+<br />
+<span class="rule-no"><i class="fa fa-thumbs-down" aria-hidden="true"></i> You can't bring a character under level 30 to block other players from casting magic walls, untrap players or move items in a war.</span><br />
+<span class="rule-no"><i class="fa fa-thumbs-down" aria-hidden="true"></i> You can't bring more then one character to a fight for any reason.</span><br />
+					<br />
+				</span>
+			</div>
+
+			<label for="post-2" class="read-more-trigger"></label>
+		</div>
+		<hr class="rule-divider" />
+		
+
+		<div>
+			<input type="checkbox" class="read-more-state" id="post-3" />
+			<font class="rule-title">Bug / Exploits</font><br />
+			<font class="rule-description">&#x2022; Do not exploit bugs or abuse the unintended consequences of game mechanics. If you find a bug or exploit you must report it to staff immediately. Using a bug to your advantage is a bannable offense.</font>
+			<div class="read-more-wrap">
+				<span class="read-more-target"><br />
+					
+<span class="rule-yes"><i class="fa fa-thumbs-up" aria-hidden="true"></i> You can be rewarded premium points for major bug reports.</span><br />
+<span class="rule-no"><i class="fa fa-thumbs-down" aria-hidden="true"></i> If you are caught using a bug to your advantage then you will be banned.</span><br />
+					<br />
+				</span>
+			</div>
+
+			<label for="post-3" class="read-more-trigger"></label>
+		</div>
+		<hr class="rule-divider" />
+		
+
+		<div>
+			<input type="checkbox" class="read-more-state" id="post-4" />
+			<font class="rule-title">Griefing / Player Harassment</font><br />
+			<font class="rule-description">&#x2022; Do not grief players, or act in a way solely and specifically intended to harass them. If a player is verbally harassing you then you should use the ignore feature.</font>
+			<div class="read-more-wrap">
+				<span class="read-more-target"><br />
+					
+<span class="rule-no"><i class="fa fa-thumbs-down" aria-hidden="true"></i> You can't manually or with a script throw items on a depot box with the intention of making another player lose their items.</span><br />
+<span class="rule-no"><i class="fa fa-thumbs-down" aria-hidden="true"></i> You can't intentionally block off any major NPC, temple, spawn or shop using creatures or unwalkable items.</span><br />
+					<br />
+				</span>
+			</div>
+
+			<label for="post-4" class="read-more-trigger"></label>
+		</div>
+		<hr class="rule-divider" />
+		
+
+		<div>
+			<input type="checkbox" class="read-more-state" id="post-5" />
+			<font class="rule-title">Illegal Trading</font><br />
+			<font class="rule-description">&#x2022; You are not allowed to trade in-game items, accounts, money or services for goods, accounts, services or money outside of our server.</font>
+			<div class="read-more-wrap">
+				<span class="read-more-target"><br />
+					
+<span class="rule-no"><i class="fa fa-thumbs-down" aria-hidden="true"></i> If your trade can't be completed inside of Verana then it's an illegal trade. Contact staff for help involving premium point trades.</span><br />
+					<br />
+				</span>
+			</div>
+
+			<label for="post-6" class="read-more-trigger"></label>
+		</div>
+		<hr class="rule-divider" />
+		
+
+		<div>
+				
+
+		<div>
+			<input type="checkbox" class="read-more-state" id="post-6" />
+			<font class="rule-title">Powerleveling / Frag Abuse</font><br />
+			<font class="rule-description">&#x2022; You are not allowed to let other players/characters repeatedly kill your character to gain experience, or kill someone who is doing the same. This includes killing your own characters to transfer experience. If you want to kill someone do so in traditional PvP, not consensual powerleveling.</font>
+			<div class="read-more-wrap">
+				<span class="read-more-target"><br />
+					
+<span class="rule-no"><i class="fa fa-thumbs-down" aria-hidden="true"></i> You can't kill your own characters or sell frags to another player in order to manipulate the top frag list.</span><br />
+<span class="rule-no"><i class="fa fa-thumbs-down" aria-hidden="true"></i> You can't repeatedly kill your own, or another character in an attempt to transfer experience between them.</span><br />
+<span class="rule-no"><i class="fa fa-thumbs-down" aria-hidden="true"></i> You can't suicide or rook your character. Repeatedly dying to players will transfer experience to the them and is considered powerleveling.</span><br />
+<span class="rule-no"><i class="fa fa-thumbs-down" aria-hidden="true"></i> If you see somebody suiciding and you kill them multiple times you will lose the experience that you gained from it.</span><br />
+					<br />
+				</span>
+			</div>
+
+			<label for="post-8" class="read-more-trigger"></label>
+		</div>
+		<hr class="rule-divider" />
+		
+
+		<div>
+			<input type="checkbox" class="read-more-state" id="post-7" />
+			<font class="rule-title">Advertising</font><br />
+			<font class="rule-description">&#x2022; Do not advertise for other servers or try to convince players to leave this server for another server. People are free to play where they want, but Verana will not act as a billboard for competing servers.</font>
+			<div class="read-more-wrap">
+				<span class="read-more-target"><br />
+					
+<span class="rule-no"><i class="fa fa-thumbs-down" aria-hidden="true"></i> Posting an IP address/website to another tibia server almost always results in a permanent ban.</span><br />
+<span class="rule-no"><i class="fa fa-thumbs-down" aria-hidden="true"></i> You are free to play other servers but if you want to recruit a team for it then you should do it on the other server.</span><br />
+<span class="rule-no"><i class="fa fa-thumbs-down" aria-hidden="true"></i> We don't want to hear how much better another server is compared to this one. Trying to convince people to leave is punishable. New servers can pay to promote their server to thousands of people using server lists but this is not the place to spread that advertisement.</span><br />
+					<br />
+				</span>
+			</div>
+
+			<label for="post-9" class="read-more-trigger"></label>
+		</div>
+		<hr class="rule-divider" />
+		
+
+		<div>
+			<input type="checkbox" class="read-more-state" id="post-9" />
+			<font class="rule-title">Scamming / Stealing Items</font><br />
+			<font class="rule-description">&#x2022; You are not allowed to scam or attempt to scam any player for items, money or accounts.</font>
+			<div class="read-more-wrap">
+				<span class="read-more-target"><br />
+					
+<span class="rule-no"><i class="fa fa-thumbs-down" aria-hidden="true"></i> If you are knowingly taking something of value from another player when they don't want to give you it then it's considered stealing. <b>Example:</b> Stealing something out of a house.</span><br />
+<span class="rule-no"><i class="fa fa-thumbs-down" aria-hidden="true"></i> Tricking a player into giving you something is considered scamming. <b>Example:</b> Running a casino and not paying out when they win.</span><br />
+<span class="rule-no"><i class="fa fa-thumbs-down" aria-hidden="true"></i> Misleading somebody to give you something is considered scamming. <b>Example:</b> Showing a player an item in a trade, canceling and putting something else instead.</span><br />
+					<br />
+				</span>
+			</div>
+
+			<label for="post-12" class="read-more-trigger"></label>
+		</div>
+		<hr class="rule-divider" />
+		<center><small>There are many possible infractions not listed in the server rules; if it were otherwise it would be an infinitely long list. As such, if a GM, CM or GOD tells you not to do something.. don't do it. If you think you've been unjustly reprimanded stop anyway and take the issue to GOD Verana.<br /><br /><b>The staff of this server reserve the right to ban for any reason not listed here and have the final decision on all banishments. If our staff feel you're hurting our server, community or website overall, we reserve the right to remove you from our server.</b></small></center></td></tr></table></div></td></tr></table></div><br /> 
+        </div>
+      </div>
+    </div>
+include 'layout/overall/footer.php'; ?>
