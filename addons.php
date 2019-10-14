8f3c542f695b4685821d732822b4ea9ac1ea5de8
H3x404 <50049926+H3x404@users.noreply.github.com>
2019-10-14 03:01:40 -0300

    ~ Addons bonus list.

---
+++ addons.php
@@ -0,0 +1,58 @@
+<?php require_once 'engine/init.php'; include 'layout/overall/header.php'; ?>
+
+<h1>General</h1>
+<hr/> 
+<center><b>Addons Bonus.</b></center> 
+<hr/> 
+
+
+<TABLE table  BGCOLOR="'.$config['site']['darkborder'].'" BORDER=0 CELLPADDING=4 CELLSPACING=1 WIDTH=100%> 
+<TR BGCOLOR="'.$config['site']['vdarkborder'].'" ><TD CLASS=white COLSPAN=5><B>Outfit Bonus Table</B></TD></TR> 
+<TR><TD><TABLE BORDER=0 CELLPADDING=2 CELLSPACING=1 WIDTH=100%> 
+<TR BGCOLOR=#eeddb9><TD><B>Outfit</B></TD><TD><B>Bonus</B></TD></TR> 
+
+<TR class="KL" BGCOLOR=#eeddb9><TD><center><b>Citizen</b><br> 
+<img src="./images/addons/1028.gif"/>&nbsp</center><br></TD><TD>+2% physical protection</TD></TR> 
+<TR  class="KL" BGCOLOR=#eeddb9><TD><center><b>Hunter</b><br> 
+<img src="./images/addons/1029.gif"/>&nbsp</center><br></TD><TD>+2 distance</TD></TR> 
+<TR  class="KL" BGCOLOR=#eeddb9><TD><center><b>Mage</b><br> 
+<img src="./images/addons/1030.gif"/>&nbsp</center><br></TD><TD>+2 magic level, +5% death protection</TD></TR> 
+<TR  class="KL" BGCOLOR=#eeddb9><TD><center><b>Knight</b><br> 
+<img src="./images/addons/1039.gif"/>&nbsp</center><br></TD><TD>+3 sword, +2 shield</TD></TR> 
+<TR  class="KL" BGCOLOR=#eeddb9><TD><center><b>Nobleman/Noblewoman</b><br> 
+<img src="./images/addons/1032.gif"/>&nbsp</center><br></TD><TD>+3% more HP</TD></TR> 
+<TR  class="KL" BGCOLOR=#eeddb9><TD><center><b>Summoner</b><br> 
+<img src="./images/addons/1033.gif"/>&nbsp</center><br></TD><TD>+1 magic level, +3% death protection</TD></TR> 
+<TR  class="KL" BGCOLOR=#eeddb9><TD><center><b>Warrior</b><br> 
+<img src="./images/addons/1034.gif"/>&nbsp</center><br></TD><TD>+3 sword, +5% physical protection</TD></TR> 
+<TR  class="KL" BGCOLOR=#eeddb9><TD><center><b>Barbarian</b><br> 
+<img src="./images/addons/1043.gif"/>&nbsp</center><br></TD><TD>+3 axe, +5% physical protection</TD></TR> 
+<TR  class="KL" BGCOLOR=#eeddb9><TD><center><b>Druid</b><br> 
+<img src="./images/addons/1044.gif"/>&nbsp</center><br></TD><TD>+1 magic level, +4% earth protection</TD></TR>
+<TR  class="KL" BGCOLOR=#eeddb9><TD><center><b>Wizard</b><br> 
+<img src="./images/addons/1045.gif"/>&nbsp</center><br></TD><TD>+1 magic level, +2% earth protection</TD></TR>  
+<TR  class="KL" BGCOLOR=#eeddb9><TD><center><b>Oriental</b><br> 
+<img src="./images/addons/1046.gif"/>&nbsp</center><br></TD><TD>+2% protection all</TD></TR> 
+<TR  class="KL" BGCOLOR=#eeddb9><TD><center><b>Pirate</b><br> 
+<img src="./images/addons/1051.gif"/>&nbsp</center><br></TD><TD>+3 club, +5% more mana</TD></TR> 
+<TR  class="KL" BGCOLOR=#eeddb9><TD><center><b>Assassin</b><br> 
+<img src="./images/addons/1052.gif"/></center><br></TD><TD>+30 speed, +3 distance</TD></TR> 
+<TR  class="KL" BGCOLOR=#eeddb9><TD><center><b>Beggar</b><br> 
+<img src="./images/addons/1053.gif"/>&nbsp</center><br></TD><TD>+5% more HP/MP</TD></TR> 
+<TR  class="KL" BGCOLOR=#eeddb9><TD><center><b>Shaman</b><br> 
+<img src="./images/addons/1054.gif"/>&nbsp</center><br></TD><TD>+1 magic level, +5% energy protection</TD></TR> 
+<TR  class="KL"  BGCOLOR=#eeddb9><TD><center><b>Norseman/Norsewoman</b><br> 
+<img src="./images/addons/1151.gif"/>&nbsp</center><br></TD><TD>+8% ice protection</TD></TR> 
+<TR  class="KL" BGCOLOR=#eeddb9><TD><center><b>Nightmare</b><br> 
+<img src="./images/addons/1168.gif"/>&nbsp</center><br></TD><TD>+5 Shielding, +5% physical protection</TD></TR>
+<TR  class="KL" BGCOLOR=#eeddb9><TD><center><b>Jester</b><br> 
+<img src="./images/addons/1058.gif"/>&nbsp</center><br></TD><TD>+2% death protection, 4% fire protection</TD></TR> 
+<TR  class="KL" BGCOLOR=#eeddb9><TD><center><b>Brotherhood</b><br> 
+<img src="./images/addons/1178.gif"/>&nbsp</center><br></TD><TD>+6% death protection</TD></TR>
+<TR  class="KL" BGCOLOR=#eeddb9><TD><center><b>Demonhunter</b><br> 
+<img src="/images/addons/1059.gif"/></center><br></TD><TD>+10% fire protection</TD></TR> 
+<TR  class="KL" BGCOLOR=#eeddb9><TD><center><b>Yalaharian</b><br> 
+<img src="/images/addons/924.gif"/></center><br></TD><TD>+3% death protection, +3% physical protection</TD></TR> 
+</table>
+
+<?php include 'layout/overall/footer.php'; ?>
